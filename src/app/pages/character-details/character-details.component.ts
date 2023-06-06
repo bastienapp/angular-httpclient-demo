b8c1394e44185b21eea27d2682ae9c95eddcdb6f
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-06-06 15:35:49 +0200

    init

--- src/app/pages/character-details/character-details.component.ts
+++ src/app/pages/character-details/character-details.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+type Character = {
+
+  id: string;
+  name: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
   styleUrls: ['./character-details.component.scss']
 })
-export class CharacterDetailsComponent {
+export class CharacterDetailsComponent implements OnInit {
+
+  characterId!: string | null;
+  characterDetail!: Character;
+
+  constructor(
+    private route: ActivatedRoute,
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .subscribe((params) => {
+        this.characterId = params.get('id');
+        this.loadCharacterById();
+      });
+  }
+
+  loadCharacterById() {
+    this.httpClient
+      .get<Character[]>(`https://hp-api.onrender.com/api/character/${this.characterId}`)
+      .subscribe((response) => {
+        this.characterDetail = response[0]
+      })
+  }
 }
